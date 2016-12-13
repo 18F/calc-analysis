8c8cbd7494bb94eac606e270d00f7b2a073139b9
Eric Schles <ericschles@gmail.com>
2016-12-13 14:50:45 -0500

    added more functionality to sin parser

--- syn_transforms/syn_transform.py
+++ syn_transforms/syn_transform.py
@@ -10,7 +10,7 @@ def parse_shortened_sin(SIN):
         new_tmp = []
         full_syn = tmp[0].split("-")[0] +"-"
         for elem in tmp:
-            if len(elem) == 1:
+            if len(elem) == 1 or len(elem) == 3:
                 new_tmp.append(full_syn+elem)
             else:
                 new_tmp.append(elem)
@@ -20,19 +20,33 @@ def parse_shortened_sin(SIN):
 
 def dealing_with_and(SIN):
     SIN = SIN.replace("and",", ")
+    SIN = SIN.replace("&",", ")
     SIN = ' '.join(SIN.split())
     SIN = SIN.replace(" , ",", ")
     return SIN
-
+    
 def replace_parens(SIN):
     #this comes from this stackoverflow:
     #http://stackoverflow.com/questions/14596884/remove-text-between-and-in-python
     return re.sub("[\(\[].*?[\)\]]", "", SIN)
 
+def iterate_sins(SIN):
+    if SIN.count("-") > 1 and "," not in SIN:
+        split_string = SIN.split("-")
+        prefix = split_string[0]
+        start = int(split_string[1])
+        end = int(split_string[2])
+        return ",".join([prefix+"-"+str(elem) for elem in range(start,end+1)])
+    return SIN
+
 def parse_sin(SIN):
+    SIN = SIN.replace("R","")
     SIN = SIN.replace("plus RC SINs","")
+    SIN = SIN.replace("SINS","")
     SIN = SIN.replace(";",",")
-    SIN = parse_shortened_sin(SIN)
+    SIN = SIN.replace("C","")
+    SIN = iterate_sins(SIN)
+    SIN = SIN.replace(" -","")
     SIN = SIN.strip()
     SIN = replace_parens(SIN)
     SIN = dealing_with_and(SIN)
@@ -42,6 +56,7 @@ def parse_sin(SIN):
     if SIN.count(" ") > 1 and SIN.count(",") == 0:
         SIN = SIN.replace(" ",", ")
     SIN = ",".join(list(set([elem.strip() for elem in SIN.split(",")])))
+    SIN = parse_shortened_sin(SIN)
     return SIN
 
 new_calc_pricing = pd.DataFrame()
