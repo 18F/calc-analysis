b325ce32ae290bf873387f20be38ebdaf5440f20
Atul Varma <varmaa@gmail.com>
2018-03-30 18:23:11 -0400

    Factor out net.ts.

---
+++ elibrary-scraper/net.ts
@@ -0,0 +1,39 @@
+import * as url from "url";
+import * as request from "request";
+
+// TODO: Get rid of this eventually.
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+const BASE_URL = 'https://www.gsaelibrary.gsa.gov';
+
+const SEARCH_RESULTS_URL = `${BASE_URL}/ElibMain/searchResults.do`;
+
+export function getContractorInfoURL(contract: string): Promise<string> {
+    return new Promise((resolve, reject) => {
+        request.get(SEARCH_RESULTS_URL, {
+            qs: {
+                searchText: contract,
+                searchType: 'exactWords',
+            },
+        }, (err, _, body) => {
+            if (err) return reject(err);
+            const $ = cheerio.load(body);
+            const a = $('a[href^="contractorInfo.do"]');
+
+            if (a.length === 1) {
+                const href = url.resolve(SEARCH_RESULTS_URL, a.attr('href'));
+                return resolve(href);
+            }
+            reject(new Error(`Invalid contract: ${contract}`));
+        });
+    });
+}
+
+export function getContractorInfoHTML(url: string): Promise<string> {
+    return new Promise((resolve, reject) => {
+        request.get(url, {}, (err, _, body) => {
+            if (err) return reject(err);
+            resolve(body);
+        });
+    });
+}
--- elibrary-scraper/scraper.ts
+++ elibrary-scraper/scraper.ts
@@ -1,50 +1,12 @@
-import * as url from "url";
-import * as request from "request";
 import * as cheerio from "cheerio";
 
 import * as cache from "./cache";
-
-// TODO: Get rid of this eventually.
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-
-const BASE_URL = 'https://www.gsaelibrary.gsa.gov';
-
-const SEARCH_RESULTS_URL = `${BASE_URL}/ElibMain/searchResults.do`;
-
-function getContractorInfoURL(contract: string): Promise<string> {
-    return new Promise((resolve, reject) => {
-        request.get(SEARCH_RESULTS_URL, {
-            qs: {
-                searchText: contract,
-                searchType: 'exactWords',
-            },
-        }, (err, _, body) => {
-            if (err) return reject(err);
-            const $ = cheerio.load(body);
-            const a = $('a[href^="contractorInfo.do"]');
-
-            if (a.length === 1) {
-                const href = url.resolve(SEARCH_RESULTS_URL, a.attr('href'));
-                return resolve(href);
-            }
-            reject(new Error(`Invalid contract: ${contract}`));
-        });
-    });
-}
-
-function getContractorInfoHTMLFromURL(url: string): Promise<string> {
-    return new Promise((resolve, reject) => {
-        request.get(url, {}, (err, _, body) => {
-            if (err) return reject(err);
-            resolve(body);
-        });
-    });
-}
+import * as net from "./net";
 
 async function getContractorInfoHTML(contract: string): Promise<string> {
-    const url = await cache.get(`url_${contract}`, () => getContractorInfoURL(contract));
+    const url = await cache.get(`url_${contract}`, () => net.getContractorInfoURL(contract));
 
-    return cache.get(`html_${contract}`, () => getContractorInfoHTMLFromURL(url));
+    return cache.get(`html_${contract}`, () => net.getContractorInfoHTML(url));
 }
 
 interface ContractorInfo {
